7c32b901026bc85648ece21faaa6cc5a69eee452
Niklas Schnelle <niks@kernel.org>
2026-07-03 22:20:31 +0200

    bazzite-niks: Add a few more packages
    Signed-off-by: Niklas Schnelle <niks@kernel.org>

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,5 +5,8 @@ set -ouex pipefail
 # Copy the contents of system_files/ of the git repo to /
 cp -avf "/ctx/system_files"/. /
 
-### Install packages
-dnf5 install -y tmux zsh htop stow neovim waypipe
+### Install missing absolute essentials
+dnf5 install -y tmux zsh htop vim neovim foot-terminfo stow
+
+### Install graphical and nice to haves
+dnf5 install -y foot waypipe
